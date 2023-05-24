4f360193892fadd354d920371e4e3efae57c4f3b
Alberto <agarnav@g.educaand.es>
2023-05-24 22:02:47 +0200

    Método Update de Sedes realizado

--- Backend/FCT_API/app/Http/Controllers/HeadquarterController.php
+++ Backend/FCT_API/app/Http/Controllers/HeadquarterController.php
@@ -121,7 +121,7 @@ class HeadquarterController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified headquarter in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -129,7 +129,102 @@ class HeadquarterController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Authentication required
+        $user = JWTAuth::parseToken()->authenticate();
+
+        if(!$user)
+        {
+            // Error invalid token
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid Token / Expired Token'
+            ], 401);
+        }
+        elseif($user->role_id != 1)
+        {
+            // Only users with role 1 can update
+            return response()->json([
+                'status' => false,
+                'message' => 'User does not have permission'
+            ], 403);
+        }
+
+        // Find the headquarter
+        $headquarter = Headquarter::find($id);
+
+        if (!$headquarter)
+        {
+            // Error user does not exist
+            return response()->json([
+                'status' => false,
+                'message' => 'Headquarter does not exist'
+            ], 404);
+        }
+
+        // Request only past data
+        $data = $request->only(
+            'name',
+            'mobile',
+            'address',
+            'town'
+        );
+
+        // Rules to validate the data
+        $rules = [
+            'name' => 'required|string|max:100',
+            'mobile' => 'required|string|min:9|max:15',
+            'address'=> 'required|string|max:255',
+            'town' => 'required|string|max:35'
+        ];
+
+        // Custom messages for validation
+        $messages = [
+            'name.required' => 'Nombre requerido.',
+            'name.string' => 'El nombre debe ser una cadena de texto.',
+            'name.max' => 'El nombre no debe superar los :max caracteres.',
+            'mobile.required' => 'Número de teléfono requerido.',
+            'mobile' => 'Número de teléfono inválido.',
+            'address.required' => 'Dirección requerida.',
+            'address.string' => 'La dirección debe ser una cadena de texto.',
+            'address.max' => 'La dirección no debe superar los :max caracteres.',
+            'town.required' => 'Localidad requerida.',
+            'town.string' => 'La localidad debe ser una cadena de texto.',
+            'town.max' => 'La localidad no debe superar los :max caracteres.'
+        ];
+
+        // Data request validation
+        $validator = Validator::make($data, $rules, $messages);
+
+        // Returning error if validation fails
+        if ($validator->fails())
+        {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->messages()
+            ], 400);
+        }
+        // Mobile Validation
+        if (!UtilsValidator::validatorMobile($request->mobile)){
+            return response()->json([
+                'status' => false,
+                'message' => ['mobile' => ['Número de teléfono inválido.']]
+            ], 400);            
+        }
+
+        // Update headquarter if validation is successful
+        $headquarter->update([
+            'name' => $request->name,
+            'mobile' => $request->mobile,
+            'address' => $request->address,
+            'town' => $request->town
+        ]);
+
+        // Return the response with the new headquarter data
+        return response()->json([
+            'status' => true,
+            'message' => 'Headquarter successfully updated',
+            'headquarter' => $headquarter
+        ], 200);
     }
 
     /**
--- Backend/FCT_API/routes/api.php
+++ Backend/FCT_API/routes/api.php
@@ -47,6 +47,6 @@ Route::group(['middleware' => ['jwt.verify']], function()
     
     // Headquarter CRUD
     Route::post('companies/{id}/headquarters', [HeadquarterController::class, 'store']);
-    /*Route::put('headquarters/{id}', [HeadquarterController::class, 'update']);*/
+    Route::put('headquarters/{id}', [HeadquarterController::class, 'update']);
     /*Route::delete('headquarters/{id}', [HeadquarterController::class, 'destroy']);*/
 });
